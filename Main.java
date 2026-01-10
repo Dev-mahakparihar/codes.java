188f003e2e9d6d7b6acaf2e2309a4dc5b0c15e5c
Dev-mahakparihar <manutanu753@gmail.com>
2026-01-10 16:30:48 +0530

    hypotenuse by direct method

--- Main.java
+++ Main.java
@@ -1,23 +1,17 @@
-import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[]args){
 
-        Scanner scanner = new Scanner(System.in);
 
-        double a;
-        double b;
+
+        double a=3;
+        double b =4;
         double c;
 
-        System.out.println("Enter the value of a: " );
-        a = scanner.nextDouble();
 
-        System.out.println("Enter the value of b: " );
-        b = scanner.nextDouble();
 
         c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
         System.out.println(c);
 
     }
-
 }
