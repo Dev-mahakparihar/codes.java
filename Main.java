214c77c12a871546953e8042b25887feb707012a
Dev-mahakparihar <manutanu753@gmail.com>
2026-01-10 16:29:21 +0530

    hypotenuse by user input

--- Main.java
+++ Main.java
@@ -1,12 +1,23 @@
-public class Main{
-    public static void main(String[]args){
+import java.util.Scanner;
 
-        double a=4;
-        double b =4;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double a;
+        double b;
         double c;
+
+        System.out.println("Enter the value of a: " );
+        a = scanner.nextDouble();
+
+        System.out.println("Enter the value of b: " );
+        b = scanner.nextDouble();
 
         c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
         System.out.println(c);
 
     }
+
 }
