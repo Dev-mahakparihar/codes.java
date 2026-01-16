10fa09dee81b76cfbd7fb7dfcf620eac00b2a85d
Dev-mahakparihar <manutanu753@gmail.com>
2026-01-16 12:12:28 +0530

    rows/columns

--- Main.java
+++ Main.java
@@ -1,10 +1,25 @@
-public class Main {
-    public static void main(String[] args){
-        for (int i=1;i<=4;i++){
-        for(int j=1;j<=5;j++) {
-            System.out.print(j + " ");
-        }
+import java.util.Scanner;
+public class Main{
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+
+        int rows;
+        int columns;
+        char symbol;
+
+        System.out.print("Enter the # of rows-");
+        rows= scanner.nextInt();
+        System.out.print("Enter the # of columns-");
+        columns= scanner.nextInt();
+        System.out.print("Enter the  symbol-");
+        symbol= scanner.next().charAt(0);
+
+        for(int i =0;i<rows;i++){
+            for(int j=0;j<columns;j++){
+                System.out.print(symbol);
+            }
             System.out.println();
         }
+
     }
 }
