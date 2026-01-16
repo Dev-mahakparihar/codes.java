34d53baaed131e89a7aae330efe48ec5f59bcc57
Dev-mahakparihar <manutanu753@gmail.com>
2026-01-16 12:02:58 +0530

    matrice

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            if (i == 6) {
-                continue;
-            }
-            System.out.print(i + " ");
+    public static void main(String[] args){
+        for (int i=1;i<=4;i++){
+        for(int j=1;j<=5;j++) {
+            System.out.print(j + " ");
+        }
+            System.out.println();
         }
     }
 }
