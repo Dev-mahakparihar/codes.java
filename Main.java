8ef9a074386050793afbf1aa677a3b760e701f98
Dev-mahakparihar <manutanu753@gmail.com>
2026-01-16 12:35:10 +0530

    myname

--- Main.java
+++ Main.java
@@ -1,15 +1,10 @@
 public class Main{
     public static void main(String[]args){
-        System.out.println("Que- Find square/cube of 4.");
-        System.out.print("Ans-");
-        System.out.println("the square is " + square(4));
-        System.out.println("    the cube is " + cube(4));
+       String myfullname= fullname("Mahak", "Parihar");
+        System.out.println(myfullname);
+    }
+    static String fullname(String name, String surname){
+        return name + " " + surname;
+    }
 
-    }
-    static double cube(double number){
-        return number*number*number;
-    }
-   static double square(double number){
-        return number*number;
-    }
 }
