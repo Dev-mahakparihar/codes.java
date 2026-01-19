7de7a3fdfb79e014f606166299658a62a120ef46
Dev-mahakparihar <manutanu753@gmail.com>
2026-01-19 14:18:49 +0530

    array.1

--- Main.java
+++ Main.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
 public class Main{
     public static void main(String[]args){
-       String mypizza = makepizza("thicc crust ");
-        System.out.println(mypizza);
-    }
+        Scanner scanner = new Scanner(System.in);
 
-    static String makepizza(String bread){
-        return bread + "Pizza";
-    }
+        String[] animals= new String[4];
 
+
+        for(int i=0;i< animals.length;i++){
+            System.out.print("Enter  animal: ");
+            animals[i] = scanner.nextLine();
+
+        }
+        for(String animal:animals) {
+            System.out.println(animal);
+        }
+
+
+
+
+
+    }
 }
