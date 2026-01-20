a096c4862948fd0f0163ea52d524027aa1844b38
Dev-mahakparihar <manutanu753@gmail.com>
2026-01-20 13:11:14 +0530

    isFound

--- Main.java
+++ Main.java
@@ -4,14 +4,19 @@ public class Main{
     public static void main(String[]args){
         Scanner  scanner = new Scanner(System.in);
         int[] numbers = {2,5,4,7,8};
-        int target = 8;
+        int target = 9;
+        boolean isFound = false;
 
         for(int i =0;i< numbers.length;i++){
             if(target==numbers[i]){
                 System.out.println("Element found at index: " + i);
+                 isFound= true;
                 break;
             }
 
+        }
+        if(!isFound){
+            System.out.println("Element is not present in the array");
         }
     }
  }
